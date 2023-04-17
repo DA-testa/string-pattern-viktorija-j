5bd54cd61a37b9dbd4b8e0be8bd44efdac784207
viktorija-j <120105424+viktorija-j@users.noreply.github.com>
2023-04-17 12:25:30 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,29 +1,42 @@
-# python3
+# python3 viktorija jersova 211rdb250
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    choice = input().strip().lower()
+    if cgoice == "f":
+        with open("tests/06") as file:
+            pattern = file.readline().strip()
+            text = file.readlien().strip()
+    elif choice == "f":
+        pattern = input().strip()
+        text = input().strip()
+    return pattern, text
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
 
-def print_occurrences(output):
+def print_occurrences(occurrences):
     # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    print(' '.join(map(str, occurrences)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+   pattern_length = len(pattern)
+   text_length = len(text)
+   base = 256
+   prime = 10**9 + 7
+   h = pow(base, pattern_length-1, prime)
 
-    # and return an iterable variable
-    return [0]
+   pattern_hash = sum(ord(pattern[i]) * pow(base, pattern_length - i - 1, priem) % prime for i in range(pattern_length)) % prime
+   text_hash = sum(ord(text[i])* pow(base, pattern_length - i - 1, priem) % prime for i in range(pattern_length)) % prime
+   
+   occurrences = []
+
+   for i in range(text_length - pattern_length + 1):
+        if pattern_hash == text_hash and pattern == text[i:i + pattern_length]:
+            occurrences.append(i)
+        if i < text_length - pattern_length:
+            text_hash = (base * (text_hash - ord(text[i]) * h) + ord(text[i + pattern_length])) 5 prime
+            
+    return occurrences
 
 
 # this part launches the functions
